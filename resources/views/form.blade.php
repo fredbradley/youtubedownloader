05ba81a9ed3b6aded7f259eb6fe5da7cc1732b38
Fred Bradley <hello@fredbradley.co.uk>
2025-02-11 10:31:29 +0000

    release version

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,7 +2,7 @@
     <form action="{{ route('show') }}" method="POST" class="space-y-4">
         @csrf
         <input type="url" name="url" required placeholder="Enter YouTube URL"
-               class="w-full p-3 border rounded-lg focus:ring-2 focus:ring-blue-500 outline-none">
+               class="w-full p-3 border rounded-lg focus:ring-2 focus:ring-blue-500 outline-none dark:text-black">
 
         <button type="submit"
                 class="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 rounded-lg transition">
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,24 +1,23 @@
 @php use Illuminate\Support\Facades\File; @endphp
 <x-layout>
-    <table class="table">
+    <table class="table m-auto py-3">
         <thead>
-        <th>Filename</th>
-        <th>Download</th>
+        <th class="dark:text-black">Filename</th>
+        <th class="dark:text-black">Download</th>
         </thead>
         <tbody>
         @foreach ($files as $file)
             <tr>
-                <td>{{ File::basename($file) }}</td>
+                <td class="dark:text-black">{{ File::basename($file) }}</td>
                 <td>
                     <a href="{{ route('download', ['file' => File::basename($file)]) }}"
-                       class="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold p-3 rounded-lg transition">
+                       class="block w-full bg-blue-500 hover:bg-blue-600 text-white font-bold p-3 rounded-lg transition">
                         Download
                     </a>
             </tr>
         @endforeach
         </tbody>
     </table>
-    <hr />
     <div class="py-6">
     <a href="{{ route('form') }}"
        class="w-full block bg-green-500 hover:bg-green-600 text-white font-bold p-3 rounded-lg transition text-center">
